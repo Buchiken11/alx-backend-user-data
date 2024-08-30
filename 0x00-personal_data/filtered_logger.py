a224d159d28f3c48ffc764ef0f8c9034dc2d5e0c
Buchiken11 <buchiken025@gmail.com>
2024-08-30 08:30:21 +0100

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env ptrhon3
+
+'''
+writting logging
+'''
+import re
+from typing import list
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    pattern = f"({'|'.join(fields)})=[^{separator}]+"
+    return re.sub(pattern, lambda:m f"{m.group(1)}={redaction}", message)
