4d2e3ae5d70364fb06453ad964ad75ae5458ad93
Buchiken11 <buchiken025@gmail.com>
2024-08-30 10:22:13 +0100

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,8 @@ writting logging
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     pattern = f"({'|'.join(fields)})=[^{separator}]+"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
