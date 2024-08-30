a8477c38dccac11dfcbbb107f40c8238adc992f1
Buchiken11 <buchiken025@gmail.com>
2024-08-30 12:47:25 +0100

    obfuscation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,8 +9,15 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    # Escape separator to handle special characters in the separator (e.g., '|', '.', etc.)
+    # Escape separator to handle special characters
+    # in the separator (e.g., '|', '.', etc.)
     escaped_separator = re.escape(separator)
-    pattern = rf"(?P<field>{'|'.join(map(re.escape, fields))})=[^{escaped_separator}]+"
-    return re.sub(pattern, lambda m: f"{m.group('field')}={redaction}", message)
-
+    pattern = (rf"(?P<field>{'|'.join(map(re.escape, fields))})="
+               rf"[^{escaped_separator}]+"
+               )
+    return re.sub(
+            pattern,
+            lambda m:
+            f"{m.group('field')}={redaction}",
+            message
+            )
