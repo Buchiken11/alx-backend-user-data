899dfa347823f1b1a83857aabd845bedc13d3c9d
Buchiken11 <buchiken025@gmail.com>
2024-09-03 21:37:06 +0100

    creating mehods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,8 +16,16 @@ class Auth:
             path: the paths to be checked
             excluded_paths: paths to be excluded
         '''
-        return False
-
+        if path == None:
+            return True
+        if excluded_paths == None or len(excluded_paths) == 0:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+            for excluded_path in excluded_paths:
+                if not excluded_path.endswith('/') or excluded_path == path:
+                    return True
+                return False
 
     def authorization_header(self, request=None) -> str:
         '''
@@ -26,11 +34,9 @@ class Auth:
         '''
         return None
 
-
     def current_user(self, request=None) -> TypeVar('User'):
         '''
         def current_user(): public method
         Returns: None
         '''
         return None
-
