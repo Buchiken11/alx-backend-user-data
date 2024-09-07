7ec97cfefc94c95b2812758663823599644935bd
Buchiken11 <buchiken025@gmail.com>
2024-09-07 04:17:42 +0100

    auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"This module contains the Auth class, which provides methods to handle
+"""
+This module contains the Auth class, which provides methods to handle
 authorization and authentication logic for the API.
 """
 
@@ -9,38 +10,36 @@ from typing import List, TypeVar
 
 class Auth:
     """
-    Auth class to manage authentication
-    with public methods
+    Auth class to manage authentication with public methods
     """
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        returns False - path and excluded_paths
-        will be used later
+        Returns False - path and excluded_paths will be used later
         Args:
-            path- the route
-            excluded_path - the routes that is not contained in path.
-            Returns: False
-
+            path: The route
+            excluded_paths: The routes that are not contained in path.
+        Returns:
+            bool: False
         """
         return False
 
     def authorization_header(self, request=None) -> str:
         """
-        A public method to get the authorization
-        from the request
+        A public method to get the authorization header from the request.
+
         Args:
-            request: Request to extract the header from
-            Returns: None
+            request: Request to extract the header from.
+        Returns:
+            str: The authorization header or None if not present.
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
-        A public method that retrieves the current user
-        from the request
+        A public method that retrieves the current user from the request.
         Args:
-            request:- the Flask request object 
-            Returns:- None
+            request: The Flask request object.
+        Returns:
+            User: The current user or None if not present.
         """
         return None
