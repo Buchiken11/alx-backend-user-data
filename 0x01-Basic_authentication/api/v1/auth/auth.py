884bc46ec02614721e6eef8c309c663981fda1f1
Buchiken11 <buchiken025@gmail.com>
2024-09-07 03:07:05 +0100

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,15 +7,12 @@ from typing import List, TypeVar
 class Auth:
     """
     Auth class to manage authentication
-    Purpose: This class is the template for all
-        authentication system you will implement.
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         returns False - path and excluded_paths
         will be used later
-        now, you don’t need to take care of them
         """
         return False
 
