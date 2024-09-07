66074dbfec50c52bde946aedea7beac81dcbfb44
Buchiken11 <buchiken025@gmail.com>
2024-09-07 03:03:27 +0100

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,1 +1,34 @@
+#!/usr/bin/env pythone
 
+from flask import Flask
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Auth class to manage authentication
+    Purpose: This class is the template for all
+        authentication system you will implement.
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns False - path and excluded_paths
+        will be used later
+        now, you don’t need to take care of them
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        that returns None - request will be the
+        Flask request object
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        that returns None - request will be
+        the Flask request object
+        """
+        return None
