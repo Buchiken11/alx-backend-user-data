ce4eba0aed513709d1d8a68acd62fbc86f7e9b5f
Buchiken11 <buchiken025@gmail.com>
2024-09-07 05:12:24 +0100

    auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,6 +6,7 @@ authorization and authentication logic for the API.
 
 from flask import Flask
 from typing import List, TypeVar
+import fnmatch
 
 
 class Auth:
@@ -21,7 +22,17 @@ class Auth:
         Returns:
             bool: False
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return None
+
+        for excluded in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
+            return True
 
     def authorization_header(self, request=None) -> str:
         """
@@ -32,6 +43,8 @@ class Auth:
         Returns:
             str: The authorization header or None if not present.
         """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
