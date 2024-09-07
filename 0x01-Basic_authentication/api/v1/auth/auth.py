36bc5ababf745a7fd5c0cab7d95f5e719ecfc6e8
Buchiken11 <buchiken025@gmail.com>
2024-09-07 03:35:14 +0100

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"This module contains the Auth class, which provides methods to handle
+authorization and authentication logic for the API.
+"""
 
 from flask import Flask
 from typing import List, TypeVar
@@ -14,19 +17,30 @@ class Auth:
         """
         returns False - path and excluded_paths
         will be used later
+        Args:
+            path- the route
+            excluded_path - the routes that is not contained in path.
+            Returns: False
+
         """
         return False
 
     def authorization_header(self, request=None) -> str:
         """
-        that returns None - request will be the
-        Flask request object
+        A public method to get the authorization
+        from the request
+        Args:
+            request: Request to extract the header from
+            Returns: None
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
-        that returns None - request will be
-        the Flask request object
+        A public method that retrieves the current user
+        from the request
+        Args:
+            request:- the Flask request object 
+            Returns:- None
         """
         return None
